7647f45e0948ee62d8cbd9014c6085da7ec9419d
SebastianAlexanderBergstrom <30357348+SebastianAlexanderBergstrom@users.noreply.github.com>
2018-04-06 14:33:24 +0200

    Add files via upload

--- mma_analysis_functions.R
+++ mma_analysis_functions.R
@@ -1,5 +1,15 @@
 library(ggplot2)
 metrics_for_individual_fighters <- function(data_frame,variable,bar_width){
+  # Plots overlaid histograms of the distribution of a given variable, 
+  # enabling comparison between the two time eras.
+  # 
+  # Args:
+  #   data_frame: The data frame containing the data to be used.
+  #   variable: A string which gives the variable we wish to examine with the histograms.
+  #   bar_width: A numeric which is the desired bar width of the histograms.
+  # 
+  # Returns:
+  #   Plots the overlaid histograms.
   if(variable == "Sig.Strikes"){
     type = "significant strikes"
   }
@@ -18,7 +28,14 @@ metrics_for_individual_fighters <- function(data_frame,variable,bar_width){
 }
 
 fight_time_minutes <- function(rounds,time_in_round){
-  # Computes the length of a fight in minutes
+  # Computes the length of a fight in expressed in minutes.
+  # 
+  # Args:
+  #   rounds: The round in which a fight was finished.
+  #   time_in_round: The time at which the fight was stopped in a round.
+  #   
+  # Returns:
+  #   The fight length expressed in minutes.
   ifelse(rounds == 1,
          as.numeric(substr(time_in_round,1,1))+(1/60)*as.numeric(substr(time_in_round,3,4)),
          5*(rounds-1)+as.numeric(substr(time_in_round,1,1))+
@@ -27,6 +44,13 @@ fight_time_minutes <- function(rounds,time_in_round){
 }
 
 shorten_method_names <- function(name){
+  # Abbreviates the name of the method by which a fight was finished.
+  # 
+  # Args:
+  #   name: The string which gives the name of the method.
+  # 
+  # Returns:
+  #   An abbreviated version of the method name.
   if(substr(name,1,3) == "SUB"){
     return("SUB")
   }
@@ -51,6 +75,13 @@ shorten_method_names <- function(name){
 }
 
 format_knockout_name <- function(name){
+  # Formats the specific knockout method to a shorter name.
+  # 
+  # Args:
+  #   name: A string giving the knockout type's name to be shortened.
+  # 
+  # Returns:
+  #   A shortened version of the knockout type's name.
   if(name == "KO/TKO"){
     return("KO/TKO")
   }
@@ -65,6 +96,14 @@ format_knockout_name <- function(name){
 }
 
 format_submission_name <- function(name){
+  # Formats the name of a submission so it's easier to read in plots.
+  # 
+  # Args:
+  #   name: The string consisting of the entire submission name. 
+  # 
+  # Returns:
+  #   A shortened version of the submission name. Names ending in "choke" are
+  #   shortened by removing the substring "choke".
   char_name = as.character(name)
   if(substr(char_name,1,3)=="SUB"){
     short_name = substr(char_name,4,nchar(char_name))
@@ -80,7 +119,14 @@ format_submission_name <- function(name){
   return(short_name)
 }
 
-format_decision_name2 <- function(name){
+format_decision_name <- function(name){
+  # Formats the abbreviated decision type name so it has a clearer meaning.
+  # 
+  # Args:
+  #   name: A string which is the abbreviated version of a decision type name.
+  # 
+  # Returns:
+  #   A string which gives a clearer meaning of the decision type.
   if(name == "U-DEC"){
     return("Unanimous decision")
   }
@@ -93,8 +139,15 @@ format_decision_name2 <- function(name){
 }
 
 outcomes_within_method_type <- function(type,data_frame){
-  # Shows the percentages of fights ending in a certain type of finish given a method, e.g. a specific submission
-  # or type of knockout.
+  # Shows the percentages of fights ending in a certain type of method.
+  # 
+  # Args:
+  #   type: The type of method we're interested in.
+  #   data_frame: The data frame containing the data we're interested in.
+  # 
+  # Returns:
+  #   Plots overlaid bar plots showing the percentage of fights ending by a specific
+  #   type of method given a general method.
   if(type == "KO"){
     method_specific_data_frame = data_frame[substr(data_frame$Method,1,2) == "KO",]
     method_specific_data_frame$Method = sapply(method_specific_data_frame$Method,
@@ -108,12 +161,11 @@ outcomes_within_method_type <- function(type,data_frame){
   else if(type == "Decision"){
     method_specific_data_frame = data_frame[substr(data_frame$Method,3,5)=="DEC",]
     method_specific_data_frame$Method = sapply(method_specific_data_frame$Method,
-                                               format_decision_name2)
+                                               format_decision_name)
   }
   before_announcement = method_specific_data_frame[method_specific_data_frame$before_or_after_announcement == "Before",]
   after_announcement = method_specific_data_frame[method_specific_data_frame$before_or_after_announcement == "After",]
   ggplot()+
-    # Add stat="identity" in both geom_bar() calls
     geom_bar(data = before_announcement,
              aes(x = before_announcement$Method, y = (..count..)/sum(..count..),color="Before"),
              alpha=0.5,fill="blue",position="dodge")+
